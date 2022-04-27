1a6dbdaf3133a0a96d5636d8716e3c20ccabd763
Cardenas <cardenas900oficial@hotmail.com>
2022-04-27 13:14:11 +0200

    Taller de Geometria terminado

--- figuras.js
+++ figuras.js
@@ -1,1 +1,13 @@
-console.log("Hello word!")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es : " + perimetroCuadrado);
+
+const AreaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es : " + perimetroCuadrado);
+
+
+
+
+
